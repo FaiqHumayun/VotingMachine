7554f09fd11b2fa8bbf0e7f0a4d29dfd5b7ab431
Faiq Humayun <faiq.hamayun@devsinc.com>
2022-08-23 20:41:13 +0500

    check

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :invitable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  enum user_status: { voter: 0, candidate: 1, admin: 2, super_admin: 3}
+end

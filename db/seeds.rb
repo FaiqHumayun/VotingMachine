5f39aa5fcb8d94b67a24cbab03a89c651c060791
Faiq Humayun <faiq.hamayun@devsinc.com>
2022-08-23 20:41:13 +0500

    check

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+User.create!([
+  name: "Faiq Humayun",
+  cnic: "3310067419771",
+  voted: false,
+  constituency_name: "NA-111",
+  user_status: 2,
+  email: "faihumayun46@gmail.com",
+  password: "123456"
+
+])
+
+p "Created #{User.count} users"

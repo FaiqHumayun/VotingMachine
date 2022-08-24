5301af60f42a9f3023f866e6f77a25ccb5384531
FaiqHumayun <faiqhumayun46@gmail.com>
2022-08-24 20:05:16 +0500

    *commited  point #1 invitation module *commited point #6 module of doc *commited point #7 module of doc *commited point #8 module of doc *commited point #9 module of doc *commited point #10 module of doc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:invite, keys: [:name, :cnic, :constituency_name, :email, :user_status])
+    
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   enum user_status: { voter: 0, candidate: 1, admin: 2, super_admin: 3}
+
+  validates :name, :presence => true
+  validates :email, :uniqueness => true, :presence => true
+  validates :cnic, :uniqueness => true, :presence => true, :length => { :is => 13 }
+  
 end

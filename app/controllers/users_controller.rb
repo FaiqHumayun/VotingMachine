eb9bc831b82f2a90db72a74fcba4d1fb639e81b5
Faiq Humayun <faiq.hamayun@devsinc.com>
2022-08-30 19:02:30 +0500

    *change in userController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,4 @@
 
 # UsersController implemented for further functionalities to be implemented in future
 class UsersController < ApplicationController
-  # before_action :configure_permitted_parameters
-
-  # protected
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:invite,
-  #                                     keys: %i[name cnic constituency_name email user_status constituency_id])
-  #   devise_parameter_sanitizer.permit(:accept_invitation, keys: [:image])
-  # end
 end

034aeb9811299df84a95b573f427b034234d45aa
FaiqHumayun <faiqhumayun46@gmail.com>
2022-08-30 20:20:51 +0500

    *commit 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 Constituency.destroy_all
+Schedule.destroy_all
 
 Constituency.create([{ name: 'NA-201' }, { name: 'NA-202' }, { name: 'NA-203' }, { name: 'NA-204' },
                      { name: 'NA-205' }])
 
+Schedule.create([
+                  election_start_time: DateTime.now,
+                  election_end_time: DateTime.tomorrow
+                ])
 User.create!([
                name: 'Faiq Humayun',
                cnic: '3310067419771',
                voted: false,
-               constituency_name: 'NA-201',
+               constituency_name: Constituency.find_by(name: 'NA-201')&.name,
                user_status: 2,
                email: 'faiqhumayun46@gmail.com',
                password: '123456',
-               constituency_id: '1'
+               constituency_id: Constituency.find_by(name: 'NA-201')&.id
              ])

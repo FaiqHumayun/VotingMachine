87b0f162ff3eed1271da6cc7e3f583acf978e3ae
FaiqHumayun <faiqhumayun46@gmail.com>
2022-08-30 11:11:24 +0500

    Cloudinary initializing

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,6 +1,6 @@
 Cloudinary.config do |config|
-  config.cloud_name = 'faiqcloud'
-  config.api_key = '126273764649458'
-  config.api_secret = 'TdBiKQpFeEq40Ca5HcS8fH5tZ00'
+  config.cloud_name = ENV["faiqcloud"]
+  config.api_key = ENV["126273764649458"]
+  config.api_secret =  ENV["TdBiKQpFeEq40Ca5HcS8fH5tZ00"]
   config.secure = true
 end

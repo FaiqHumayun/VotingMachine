ef836f87ee1989fa6afb2269eec473b69ce5f81d
Faiq Humayun <faiq.hamayun@devsinc.com>
2022-08-31 14:30:19 +0500

    *pundit update 2

--- app/controllers/candidateslists_controller.rb
+++ app/controllers/candidateslists_controller.rb
@@ -3,6 +3,6 @@
 # CandidateslistsController controller
 class CandidateslistsController < ApplicationController
   def index
-    @userlists = User.all.select { |user| user.candidate? && user.constituency_name == current_user.constituency_name }
+    @userlists = User.all.select { |user| (user.candidate? || user.super_admin?) && user.constituency_name == current_user.constituency_name }
   end
 end

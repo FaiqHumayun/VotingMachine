43ff385490b4d0cdc9fe6655adce746895e5626f
FaiqHumayun <faiqhumayun46@gmail.com>
2022-08-31 11:57:31 +0500

    *Updates in schedule module

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -6,21 +6,8 @@ class SchedulesController < ApplicationController
     @schedules = Schedule.all
   end
 
-  def new
-    @schedule = Schedule.new
-  end
-
   def edit
     @schedule = Schedule.find(params[:id])
-  end
-
-  def create
-    @schedule = Schedule.new(schedule_params)
-    if @schedule.save
-      redirect_to schedules_path
-    else
-      render 'new'
-    end
   end
 
   def update

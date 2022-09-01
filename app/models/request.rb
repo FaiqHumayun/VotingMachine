9ee6ca13f5b79483b3674ff5ba46e31352a86497
FaiqHumayun <faiqhumayun46@gmail.com>
2022-09-01 17:37:51 +0500

    *rubocop

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,7 +2,7 @@
 
 class Request < ApplicationRecord
   has_one_attached :avatar
-  
+
   validates :name, presence: true
   validates :cnic, presence: true, length: { is: 13 }
   enum request_status: { pending: 0, approved: 1, rejected: 2 }

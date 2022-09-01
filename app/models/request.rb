ce90ce2cb5c4f9448c0c1336cee9e4ca5afea109
FaiqHumayun <faiqhumayun46@gmail.com>
2022-09-01 16:50:32 +0500

    *rubocop changes

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Request < ApplicationRecord
+  has_one_attached :avatar
+  
   validates :name, presence: true
   validates :cnic, presence: true, length: { is: 13 }
   enum request_status: { pending: 0, approved: 1, rejected: 2 }
-
-  has_one_attached :avatar
 end
